3599b6364d7bd0435e93b38aeebe7fb1f073cc9a
Mayur Gohane <mayur100888@gmail.com>
2024-09-27 08:40:44 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Credit Risk Assessment System",
+    version="0.0.0",
+    author="Bappy",
+    author_email="mayurgohane19@gmail.com",
+    packages=find_packages()
+)
